0797fc526bb4bfd0202bae5cb6ba1eddd9ff5333
Jorge Ricardo Soares <jorgersoares@outlook.com>
2021-04-06 02:25:04 -0300

    adicionando tela de notas

---
+++ src/main/java/br/edu/ifpb/sistema_academico/beans/FormNotasBean.java
@@ -0,0 +1,52 @@
+package br.edu.ifpb.sistema_academico.beans;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.edu.ifpb.sistema_academico.models.Estudante;
+import br.edu.ifpb.sistema_academico.services.EstudanteService;
+
+@Named
+@ViewScoped
+public class FormNotasBean implements Serializable {
+	
+	@Inject
+	private EstudanteService service;
+
+	public String atualizarBoletim(Estudante estudante) {
+		this.getFlash().setKeepMessages(true);
+		service.alterar(estudante);
+		
+		FacesMessage mensagem = new FacesMessage(
+		          FacesMessage.SEVERITY_INFO,
+		          "Boletim atualizado com sucesso!",
+		          null);
+		      getContext().addMessage(null, mensagem);
+		
+		return "notas?faces-redirect=true";
+	}
+	
+	public boolean liberaCampoFinal(Estudante estudante) {
+		if(estudante.getNota1() != null && estudante.getNota2() != null && estudante.getNota3() != null) {
+			return !((
+					estudante.getNota1().doubleValue() + 
+					estudante.getNota2().doubleValue() + 
+					estudante.getNota3().doubleValue())/3 >= 4);
+		}
+		return true;
+	}
+	
+	private Flash getFlash() {
+	    return FacesContext.getCurrentInstance().getExternalContext().getFlash();
+	}
+	
+	private FacesContext getContext() {
+		return FacesContext.getCurrentInstance();
+	}
+}
